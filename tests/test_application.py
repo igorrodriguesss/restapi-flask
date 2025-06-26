d55017f7999a50e431e82c27ed6bd7af14c20a41
Igor Rodrigues <igorr2693@gmail.com>
2025-06-26 19:35:19 -0300

    test: testing post user

--- tests/test_application.py
+++ tests/test_application.py
@@ -9,6 +9,36 @@ class TestApplication():
         app = create_app('config.MockConfig')
         yield app.test_client()
 
+    @pytest.fixture
+    def valid_user(self):
+        return {
+            "first_name": "Igor",
+            "last_name": "Rodrigues",
+            "cpf": "575.514.350-18",
+            "email": "igorr2693@gmail.com",
+            "birth_date": "2002-04-23",
+        }
+    
+
+    @pytest.fixture
+    def invalid_user(self):
+        return {
+            "first_name": "Igor",
+            "last_name": "Rodrigues",
+            "cpf": "575.514.350-12",
+            "email": "igorr2693@gmail.com",
+            "birth_date": "2002-04-23",
+        }
+
     def test_get_users(self, client):
         response = client.get('/users')
         assert response.status_code == 200
+
+    def test_post_user(self, client, valid_user, invalid_user):
+        response = client.post('/user', json=valid_user)
+        assert response.status_code == 200
+        assert b"successfully" in response.data
+
+        response = client.post('/user', json=invalid_user)
+        assert response.status_code == 400
+        assert b"invalid" in response.data
